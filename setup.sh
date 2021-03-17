2b5199d9c5aab9b489b9c9a1d1d6e7942e5b8c3e
Chris Potter <christhepcguy@gmail.com>
2021-03-17 18:45:18 -0400

    Add installation script

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+echo "----------------------------------------------"
+echo "-- Pi Information Collector Client Installs --"
+echo "----------------------------------------------"
+
+echo "----------------------------------------------"
+echo "Updating APT and installing dependent packages"
+echo "----------------------------------------------"
+sudo apt-get install git python3-pip
+
+echo "----------------------------------------------"
+echo "Installing additional python libraries"
+echo "----------------------------------------------"
+pip3 install pika psutil netifaces
+
+echo "----------------------------------------------"
+echo "Downloading scripts from git repo"
+echo "----------------------------------------------"
+git clone https://github.com/noitcerid/pi_info_mq.git /home/pi/pi_info_mq
+
+echo "----------------------------------------------"
+echo "Setting file permissions on scripts"
+echo "----------------------------------------------"
+chmod a+x /home/pi/pi_info_mq/client.py
+
+echo "----------------------------------------------"
+echo "Setting up Cron job to regularly execute client script"
+echo "----------------------------------------------"
+echo "5 * * * * /usr/bin/python3 /home/pi/pi_info_mq/client.py" | sudo tee /etc/cron.d/pi_info_mq
+
+echo "----------------------------------------------"
+echo "--      Client installation complete!       --"
+echo "----------------------------------------------"
