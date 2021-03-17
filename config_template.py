671582d6325961f193e58bbb29b1c006d3a09ad4
Chris Potter <christhepcguy@gmail.com>
2021-03-17 18:44:38 -0400

    Add config template

---
+++ config_template.py
@@ -0,0 +1,8 @@
+# Store configuration information in a consistent location
+mq_server = 'my_rabbitmq_server'
+queue_name = 'my_queue'
+exchange = ''
+db_host = 'my_server'
+db_name = 'my_database'
+db_user = 'db_user'
+db_password = 'db_password'
